643e30655fb7ac371abd4a54c29d04ba115db2da
Vinyas N S <vinyas.n.s@gmail.com>
2017-08-08 23:50:41 +0530

    Basic structuring

---
+++ home/urls.py
@@ -0,0 +1,8 @@
+# home/urls.py
+from django.conf.urls import url
+from home import views
+
+urlpatterns = [
+    url(r'^$', views.HomePageView.as_view()),
+    url(r'^about/$', views.AboutPageView.as_view()),
+]
